3aa8cacafee2920e9d43ff95ca7982ea9ad7181c
Oleg Shuruev <shuruev@gmail.com>
2016-08-23 09:47:09 -0700

    Added separate module with JSON-serialized configuration

---
+++ Atom.Module.Configuration.Json/ConfigParserJson.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace Atom.Toolbox
+{
+	/// <summary>
+	/// Parses configuration values.
+	/// </summary>
+	public static class ConfigParserJson
+	{
+		/// <summary>
+		/// Gets configuration value of specified type, assuming its value serialized using JSON.
+		/// Returns default value if specified value is not set.
+		/// </summary>
+		public static T GetJson<T>(this IConfigReader reader, string name, T defaultValue)
+		{
+			var value = reader.GetValue(name);
+			if (value == null)
+				return defaultValue;
+
+			return ParseJson<T>(name, value);
+		}
+
+		/// <summary>
+		/// Gets configuration value of specified type, assuming its value serialized using JSON.
+		/// Throws exception if specified value is not set.
+		/// </summary>
+		public static T GetJson<T>(this IConfigReader reader, string name)
+		{
+			var value = reader.GetValue(name);
+			if (value == null)
+			{
+				string message = $"Configuration parameter '{name}' is not found.";
+				throw new ConfigurationErrorsException(message);
+			}
+
+			return ParseJson<T>(name, value);
+		}
+
+		/// <summary>
+		/// Parses configuration value of specified type, assuming its value serialized using JSON.
+		/// </summary>
+		private static T ParseJson<T>(string name, string value)
+		{
+			if (value == null)
+				throw new ArgumentNullException(nameof(value));
+
+			try
+			{
+				return JsonConvert.DeserializeObject<T>(value);
+			}
+			catch
+			{
+				throw new ConfigurationErrorsException(
+					$"Configuration parameter '{name}' should contain JSON value of type {typeof(T).Name}.");
+			}
+		}
+	}
+}
--- build/build.bat
+++ build/build.bat
@@ -1,2 +1,6 @@
-nuget.exe pack "..\Atom.Module.Base64Url\Atom.Module.Base64Url.csproj" -Build -Properties Configuration=Release -Verbosity Normal
+nuget.exe pack "..\Atom.Module.Base64Url\Atom.Module.Base64Url.csproj" -Build -Properties Configuration=Release
+PAUSE
+nuget.exe pack "..\Atom.Module.Configuration\Atom.Module.Configuration.csproj" -Build -Properties Configuration=Release
+PAUSE
+nuget.exe pack "..\Atom.Module.Configuration.Json\Atom.Module.Configuration.Json.csproj" -IncludeReferencedProjects -Build -Properties Configuration=Release
 PAUSE
